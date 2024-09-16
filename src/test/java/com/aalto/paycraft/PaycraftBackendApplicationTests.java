6ea9e28910180fa23e40e8b498df08bd3bf1f064
devhnry <devthenry@gmail.com>
2024-09-16 17:46:16 +0100

    build: project Bootstrap

---
+++ src/test/java/com/aalto/paycraft/PaycraftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aalto.paycraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaycraftBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
