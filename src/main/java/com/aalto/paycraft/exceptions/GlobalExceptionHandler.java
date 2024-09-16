0fa244504b8a7ab046f3ac35e7cd5729431fe8c5
devhnry <devthenry@gmail.com>
2024-09-16 18:49:41 +0100

    feat: implemented EmployerDetailService and a GlobalExceptionHandler

---
+++ src/main/java/com/aalto/paycraft/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.aalto.paycraft.exceptions;
+
+import com.aalto.paycraft.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.time.LocalDateTime;
+
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /* handles all exceptions */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest){
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
