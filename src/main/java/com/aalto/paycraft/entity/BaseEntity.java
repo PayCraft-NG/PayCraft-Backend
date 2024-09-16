b8d0ebebe3e30440037915879218a5af32fd782c
devhnry <devthenry@gmail.com>
2024-09-16 18:30:31 +0100

    feat: created BaseEntity and AuditAwareImplementation

---
+++ src/main/java/com/aalto/paycraft/entity/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.aalto.paycraft.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass /* Informs JPA that this is a super entity that will be extended by other entities. */
+@Getter @Setter @ToString
+@EntityListeners(AuditingEntityListener.class) /* basically to handle @ModifiedBy */
+public class BaseEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    private String updatedBy;
+}
