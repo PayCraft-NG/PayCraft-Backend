6f2687b35fb1c4ee0260bc969184fc87fbf388cb
3akare <bakaredavid007@gmail.com>
2024-09-16 20:06:05 +0100

    feat (EmployerDTO.java, EmployerPasswordUpdateDTO.java): update employer data tranfer objects

--- src/main/java/com/aalto/paycraft/dto/EmployerDTO.java
+++ src/main/java/com/aalto/paycraft/dto/EmployerDTO.java
@@ -1,4 +1,60 @@
 package com.aalto.paycraft.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EmployerDTO {
+
+    @Size(min = 3, max = 100)
+    @NotEmpty(message = "First name cannot be null or empty")
+    private String firstName;
+
+    @Size(min = 3, max = 100)
+    @NotEmpty(message = "Last name cannot be null or empty")
+    private String lastName;
+
+    @Email(message = "Email address format is invalid")
+    @NotEmpty(message = "Email address cannot be null or empty")
+    private String emailAddress;
+
+    @NotEmpty(message = "Phone number cannot be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{13})", message = "Phone number must be 13 digits")
+    private String phoneNumber;
+
+    @NotEmpty(message = "Street address cannot be null or empty")
+    private String streetAddress;
+
+    @Size(min = 3, max = 100)
+    @NotEmpty(message = "Job title cannot be null or empty")
+    private String jobTitle;
+
+    @NotEmpty(message = "BVN cannot be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{11})", message = "BVN must be 11 digits")
+    private String bvn;
+
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @NotEmpty(message = "Password cannot be null or empty")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    )
+    private String password;
+
+    private UUID employerId;
 }
