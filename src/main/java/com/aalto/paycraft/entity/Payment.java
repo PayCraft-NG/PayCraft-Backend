693fa97141a225ccea25754fd2cda66e2c5b29fa
3akare <bakaredavid007@gmail.com>
2024-09-18 14:49:55 +0100

    feat: add payment entity

---
+++ src/main/java/com/aalto/paycraft/entity/Payment.java
@@ -0,0 +1,44 @@
+package com.aalto.paycraft.entity;
+
+import com.aalto.paycraft.dto.enumeration.PaymentMethod;
+import com.aalto.paycraft.dto.enumeration.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import java.sql.Types;
+import java.util.UUID;
+import java.time.LocalDate;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "Payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    @JdbcTypeCode(Types.VARCHAR)
+    private UUID paymentId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentMethod paymentMethod;
+
+    @Column(nullable = false)
+    private LocalDate paymentDate;
+
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus paymentStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "payrollId", referencedColumnName = "payrollId", nullable = false)
+    private Payroll payroll;
+}
