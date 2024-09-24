149d0fbe4f6b40b7e128f87c776924fb87c02dac
devhnry <devthenry@gmail.com>
2024-09-24 19:19:06 +0100

    feat: implemented method to make transfer into virtual account

--- src/main/java/com/aalto/paycraft/service/IWalletService.java
+++ src/main/java/com/aalto/paycraft/service/IWalletService.java
@@ -1,8 +1,16 @@
 package com.aalto.paycraft.service;
 
+import com.aalto.paycraft.dto.BankTransferResponseDTO;
+import com.aalto.paycraft.dto.DefaultKoraResponse;
+import com.aalto.paycraft.dto.VirtualAccountResponseDTO;
 import com.aalto.paycraft.entity.Employer;
 import com.aalto.paycraft.entity.VirtualAccount;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
 
 public interface IWalletService {
     VirtualAccount createVirtualAccount(Employer employer);
+    // New method to initiate a bank transfer
+    DefaultKoraResponse<BankTransferResponseDTO> initiateBankTransfer(BigDecimal amount, String currency, Employer employer);
 }
