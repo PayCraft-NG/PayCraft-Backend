4f31520161d77332afb41794a53208650c7b7b62
devhnry <devthenry@gmail.com>
2024-09-27 23:36:11 +0100

    refactor: entity update

--- src/main/java/com/aalto/paycraft/repository/WebhookDataRepository.java
+++ src/main/java/com/aalto/paycraft/repository/WebhookDataRepository.java
@@ -3,10 +3,10 @@ package com.aalto.paycraft.repository;
 import com.aalto.paycraft.entity.WebhookData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
 public interface WebhookDataRepository extends JpaRepository<WebhookData, Long> {
+    // Get WebHook Data by the Reference Number
     Optional<WebhookData> findByReference(String reference);
 }
