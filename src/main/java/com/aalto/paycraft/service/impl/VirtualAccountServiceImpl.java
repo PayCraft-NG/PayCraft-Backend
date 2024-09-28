c4f41aa566530d0649e8441a80e6ba3681b3b8e5
3akare <bakaredavid007@gmail.com>
2024-09-28 12:33:07 +0100

    refactor: code refactor

--- src/main/java/com/aalto/paycraft/service/impl/VirtualAccountServiceImpl.java
+++ src/main/java/com/aalto/paycraft/service/impl/VirtualAccountServiceImpl.java
@@ -120,6 +120,18 @@ public class VirtualAccountServiceImpl implements IVirtualAccountService {
                         .employerId(String.valueOf(EMPLOYER().getEmployerId()))
                         .build();
 
+                //====== Email Service ======//
+                if (enableEmail){
+                    log.info("===== Email Enabled =====");
+                    emailService.sendEmail(EMPLOYER().getEmailAddress(),
+                            "Account Created",
+                            createEmailContextAccountCreated(EMPLOYER().getFirstName(), frontendUrl, data.getAccount_number()),
+                            "accountCreated");
+                }
+                else
+                    log.info("===== Email Disabled =====");
+                //====== Email Service ======//
+
                 response.setStatusCode(REQUEST_SUCCESS);
                 response.setStatusMessage("Virtual bank account created successfully");
                 response.setData(virtualAccountDTO);
@@ -131,51 +143,6 @@ public class VirtualAccountServiceImpl implements IVirtualAccountService {
         } catch (Exception e) {
             log.error("Error creating virtual account: {}", e.getMessage());
             throw new RuntimeException(e);
-        }
-
-        if(responseBody.getMessage().equals("Virtual bank account created successfully")){
-            VirtualAccountResponseDTO data = responseBody.getData();
-            virtualAccount = VirtualAccount.builder()
-                .accountNumber(data.getAccount_number())
-                .accountReference(data.getAccount_reference())
-                .koraAccountReference(data.getUnique_id())
-                .bankCode(data.getBank_code())
-                .bankName(data.getBank_name())
-                .accountStatus(data.getAccount_status())
-                .balance(BigDecimal.ZERO)
-                .currency(Currency.valueOf(data.getCurrency()))
-                .employer(EMPLOYER()).build();
-
-            VirtualAccount savedVirtualAccount = virtualAccountRepository.save(virtualAccount);
-
-            //====== Email Service ======//
-            if (enableEmail){
-                log.info("===== Email Enabled =====");
-                emailService.sendEmail(EMPLOYER().getEmailAddress(),
-                        "Account Created",
-                        createEmailContextAccountCreated(EMPLOYER().getFirstName(), frontendUrl, data.getAccount_number()),
-                        "accountCreated");
-            }
-            else
-                log.info("===== Email Disabled =====");
-            //====== Email Service ======//
-
-            response.setStatusCode("00");
-            response.setStatusMessage("Virtual bank account created successfully");
-            virtualAccountDTO = VirtualAccountDTO.builder()
-                    .virtualAccountId(String.valueOf(savedVirtualAccount.getAccountId()))
-                    .accountNumber(savedVirtualAccount.getAccountNumber())
-                    .bankCode(savedVirtualAccount.getBankCode())
-                    .bankName(savedVirtualAccount.getBankName())
-                    .accountStatus(savedVirtualAccount.getAccountStatus())
-                    .balance(savedVirtualAccount.getBalance())
-                    .currency(savedVirtualAccount.getCurrency())
-                    .employerId(String.valueOf(EMPLOYER().getEmployerId())).build();
-            response.setData(virtualAccountDTO);
-        }else{
-            response.setStatusCode("49");
-            response.setStatusMessage("Unable to create virtual account");
-            return response;
         }
         return response;
     }
@@ -315,31 +282,6 @@ public class VirtualAccountServiceImpl implements IVirtualAccountService {
             log.error("Error initiating bank transfer: {}", e.getMessage());
             throw new RuntimeException(e);
         }
-
-        log.info("response ====> {}", responseBody);
-
-        if(responseBody.getMessage().equals("Bank transfer initiated successfully")){
-            BankTransferResponseDTO data = responseBody.getData();
-
-            BankTransferDetailsDTO detailsForTransfer = BankTransferDetailsDTO.builder()
-                    .amount(BigDecimal.valueOf(data.getAmount()))
-                    .amountExpected(BigDecimal.valueOf(data.getAmount_expected()))
-                    .referenceNumber(data.getReference())
-                    .paymentReference(data.getPayment_reference())
-                    .accountName(data.getBank_account().getAccount_name())
-                    .accountNumber(data.getBank_account().getAccount_number())
-                    .bankName(data.getBank_account().getBank_name())
-                    .expiryDate(String.valueOf(data.getBank_account().getExpiry_date_in_utc()))
-                    .build();
-
-            response.setStatusCode("00");
-            response.setStatusMessage("Bank transfer created successfully");
-            response.setData(detailsForTransfer);
-        }else {
-            response.setStatusCode("49");
-            response.setStatusMessage("Bank transfer initialization failed");
-            return response;
-        }
         return response;
     }
 
@@ -376,11 +318,10 @@ public class VirtualAccountServiceImpl implements IVirtualAccountService {
                     }
                     else
                         log.info("===== Email Disabled (credited) =====");
-
                     //====== Email Service ======//
                       
-                        response.setStatusCode("00");
-                        response.setStatusMessage("Bank transfer successful");
+                    response.setStatusCode("00");
+                    response.setStatusMessage("Bank transfer successful");
                     } else {
                         log.warn("Bank transfer failed for reference: {}", referenceNumber);
                         response.setStatusCode("49");
