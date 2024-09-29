f8afb445d9f0f79a8727321f1ae65a51c08eb147
devhnry <devwhenry@gmail.com>
2024-09-29 10:47:37 +0100

    feat: wrapped up implementation to request payout for a single employee

--- src/main/java/com/aalto/paycraft/repository/PaymentRepository.java
+++ src/main/java/com/aalto/paycraft/repository/PaymentRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -14,6 +15,6 @@ public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 
     // Method to find all payments by virtual account's ID and order by transactionDateTime desc
     Page<Payment> findAllByAccount_AccountIdOrderByTransactionDateTimeDesc(UUID accountId, Pageable pageable);
-
     List<Payment> findAllByAccount_AccountId(UUID accountId);
+    Optional<Payment> findByReferenceNumber(String referenceNumber);
 }
