b177ecdc06ccf7e1b9ed977b334eeb7d62b94d98
devhnry <devwhenry@gmail.com>
2024-09-29 21:25:56 +0100

    fix: deployment issue

--- src/main/java/com/aalto/paycraft/repository/EmployeeRepository.java
+++ src/main/java/com/aalto/paycraft/repository/EmployeeRepository.java
@@ -3,11 +3,13 @@ package com.aalto.paycraft.repository;
 import com.aalto.paycraft.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
     // Find only active employees (where deleted is false)
     @Query("SELECT e FROM Employee e WHERE e.employeeId = :employeeId AND e.deleted = false")
