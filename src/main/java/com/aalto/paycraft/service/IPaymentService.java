c8ec23d064476c051780420f08004ffb80647b9a
devhnry <devthenry@gmail.com>
2024-09-29 22:51:36 +0100

    feat: payout for employees

--- src/main/java/com/aalto/paycraft/service/IPaymentService.java
+++ src/main/java/com/aalto/paycraft/service/IPaymentService.java
@@ -3,8 +3,11 @@ package com.aalto.paycraft.service;
 import com.aalto.paycraft.dto.DefaultApiResponse;
 import com.aalto.paycraft.dto.PaymentDTO;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IPaymentService {
-    DefaultApiResponse<PaymentDTO> payEmployee(UUID employeeId);
+    DefaultApiResponse<List<String>> getBankNames() throws Exception;
+    DefaultApiResponse<?> payEmployee(UUID employeeId);
+    DefaultApiResponse<?> verifyPayment(String referenceNumber);
 }
