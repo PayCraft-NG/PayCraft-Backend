ba0ec5c796dbefa20bf85af5d99dbfe32d846dbd
devhnry <devthenry@gmail.com>
2024-09-29 21:25:56 +0100

    fix: deployment issue

--- src/main/java/com/aalto/paycraft/repository/CompanyRepository.java
+++ src/main/java/com/aalto/paycraft/repository/CompanyRepository.java
@@ -3,10 +3,12 @@ package com.aalto.paycraft.repository;
 import com.aalto.paycraft.entity.Company;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, UUID> {
     boolean existsByCompanyNameAndEmployer_EmployerId(String companyName, UUID employerId);
     List<Company> findAllByEmployer_EmployerId(UUID employerId, Pageable pageable);
