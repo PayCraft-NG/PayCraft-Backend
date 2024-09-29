571cb74d5275d85664b85af4fe7c246669ca93f9
devhnry <devthenry@gmail.com>
2024-09-29 08:07:55 +0100

    fix: updated webhook response for virtual account transactions

--- src/main/java/com/aalto/paycraft/dto/WebhookResponseDTO.java
+++ src/main/java/com/aalto/paycraft/dto/WebhookResponseDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 @JsonIgnoreProperties
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class WebhookResponseDTO {
+public class WebhookResponseDTO<T> {
     private String event;
-    private WebhookResponseData data;
+    private T data;
 }
